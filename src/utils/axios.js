542fe056e1a8233847b40a15b173f3bc13a7fcb5
CXT <cxt77965@163.com>
2020-12-04 11:48:22 +0800

    5.给publish页面添加组件内的守卫&添加监听器

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -13,7 +13,7 @@ Vue.prototype.$axios = axios
 // 设置基地址
 axios.defaults.baseURL = "http://ttapi.research.itcast.cn"
 
-axios.defaults.transformResponse= (data) => {
+axios.defaults.transformResponse = (data) => {
   // console.log(data);
   // 对 data 进行任意转换处理
   try {
@@ -29,32 +29,35 @@ axios.defaults.transformResponse= (data) => {
 }
 
 // 添加请求拦截器
-axios.interceptors.request.use( (config) => {
-    // 在发送请求之前做些什么
-    // console.log(config);
-    let user = JSON.parse(getToken())
-    // 有值才添加token
-    if(user){
-        config.headers.Authorization = `Bearer ${user.token}`
-    }
-    return config;
-  },  (error) => {
-    // 对请求错误做些什么
-    return Promise.reject(error);
-  });
+axios.interceptors.request.use((config) => {
+  // 在发送请求之前做些什么
+  // console.log(config);
+  let user = JSON.parse(getToken())
+  // 有值才添加token
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config;
+}, (error) => {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
 
 // 添加响应拦截器
-axios.interceptors.response.use( (response) => {
-    // 对响应数据做点什么
-    return response;
-  },  (error) => {
+axios.interceptors.response.use((response) => {
+  // 对响应数据做点什么
+  return response;
+}, (error) => {
+  //判断token是否有误
+  if (error.response.status == 401) {
     //提示请先登录
     Vue.prototype.$message.error('请先登录')
 
     //打回登录页
     router.push('/login')
-    // 对响应错误做点什么
-    return Promise.reject(error);
-  });
+    对响应错误做点什么
+  }
+  return Promise.reject(error);
+});
 
-  export {axios}
+export { axios }
--- src/views/home/publish/index.vue
+++ src/views/home/publish/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <el-form v-loading="isLoading" ref="form" :rules="rules" :model="form" label-width="60px">
+    <el-form
+      v-loading="isLoading"
+      ref="form"
+      :rules="rules"
+      :model="form"
+      label-width="60px"
+    >
       <el-form-item label="标题" prop="title">
         <el-input v-model="form.title" placeholder="请输入标题"></el-input>
       </el-form-item>
@@ -52,6 +58,8 @@ export default {
         channel_id: "",
       },
 
+      oldForm: {},
+
       // 表单验证规则
       rules: {
         title: [
@@ -97,10 +105,10 @@ export default {
     doPublish(formName) {
       this.$refs[formName].validate(async (valid) => {
         if (valid) {
-          if (this.$route.name == "publish-edit") { 
+          if (this.$route.name == "publish-edit") {
             //修改文章
             this.$message.warning("修改失败");
-          } else { 
+          } else {
             //新增文章
             // 发请求去做新增
             const res = await this.$axios.post("/mp/v1_0/articles", {
@@ -125,23 +133,74 @@ export default {
         }
       });
     },
-  },
 
-  async created() {
-    //判断是否修改文章
-    if (this.$route.name == "publish-edit") {
+    //封装获取id文章数据
+    async loadData() {
       //根据id获取文章数据
       const res = await this.$axios.get(
         `/mp/v1_0/articles/${this.$route.params.id}`
       );
       // console.log(res);   已查看，接口有误暂无数据
       this.form = res.data.data;
-      this.isLoading = false
+      this.oldForm = { ...this.from };
+      this.isLoading = false;
+    }
+  },
+
+   created() {
+    //判断是否修改文章
+    if (this.$route.name == "publish-edit") {
+      this.loadData()
     } else {
       //新增就不需要加载中，直接就停掉
       this.isLoading = false;
     }
   },
+
+  beforeRouteLeave(to, from, next) {
+    // 导航离开该组件的对应路由时调用
+    // 可以访问组件实例 `this`
+    if (this.$route.name == "publish-edit") {
+      if (
+        this.form.title == this.oldForm.title &&
+        this.form.content == this.oldForm.content
+      ) {
+        return next();
+      }
+    } else {
+      //新增判断是否为空
+      if (this.form.title == "" && this.form.content == "") {
+        return next();
+      }
+    }
+
+    this.$confirm("您当前页有未保存内容, 是否继续?", "提示", {
+      confirmButtonText: "确定",
+      cancelButtonText: "取消",
+      type: "warning",
+    })
+      .then(() => {
+        next()
+      })
+      .catch(() => {
+
+      });
+  },
+
+  //监听器
+  watch:{
+    //监听id的变化,value是变化后的值
+    '$route.params.id'(value) {
+      if(value) {
+        //有值，就是修改，只要把id变了，根据新id去发请求渲染页面
+        this.loadData()
+      } else {
+        //如果value没值，代表新增，就清空界面
+        this.from.title = ''
+        this.from.content = ''
+      }
+    }
+  }
 };
 </script>
 
